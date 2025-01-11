6079f7dbe14ab80d5176a6e26fc7b394a38f1667
PranjalWalunj <pranjalwalunj132@gmail.com>
2025-01-11 14:55:38 +0530

    my own world

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
  
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('hiiii Handsome ');
+ res.send('My Cuitie');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
